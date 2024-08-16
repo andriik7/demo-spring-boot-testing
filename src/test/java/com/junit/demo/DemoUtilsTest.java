0a0cc65c4a3ccdbb15a19bd9c056b68daac2ceb3
Andrii Kuchera <akuchera7@gmail.com>
2024-08-16 22:08:20 +0300

    V1.1 -- added lifecycle methods in testing class to execute some operations relating to @Test methods

--- src/test/java/com/junit/demo/DemoUtilsTest.java
+++ src/test/java/com/junit/demo/DemoUtilsTest.java
@@ -1,23 +1,42 @@
 package com.junit.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void beforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach is invoked");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("@AfterEach is invoked\n");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("@BeforeAll being executed\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("@AfterAll being executed");
+    }
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
-
         assertEquals(6, demoUtils.add(4, 2), "2+4 is 6");
         assertNotEquals(74, demoUtils.add(6, 1), "6+1 is not 74");
     }
 
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
-
         assertNull(demoUtils.checkNull(null), "Return should be null");
         assertNotNull(demoUtils.checkNull("null"), "Return shouldn't be null");
     }
